5612f9f144706db91060c4b793c76983b94c2e4c
arnaud fgr. <arnaudfgr.dev@gmail.com>
2026-03-12 22:41:06 +0100

    FIX: nom de route dynamic

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -2,10 +2,6 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event)
     const config = useRuntimeConfig()
 
-    // LOG TEMPORAIRE
-    console.log('apiBaseUrl:', config.apiBaseUrl)
-    console.log('URL appelée:', `${config.apiBaseUrl}/login`)
-
     try {
         const data = await apiFetch<AuthResponse>('/login', {
             method: 'POST',
