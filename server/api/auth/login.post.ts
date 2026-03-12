726c30756861afc8783caa843886d6910c67ef4f
arnaud fgr. <arnaudfgr.dev@gmail.com>
2026-03-12 22:02:22 +0100

    DEBUG : log pour debug la prod

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,19 +1,20 @@
-import {apiFetch} from "#server/utils/api";
-import {handleSpringError} from "#server/utils/errors";
-import {setAuthCookies} from "#server/utils/cookies";
-
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
+    const config = useRuntimeConfig()
+
+    // LOG TEMPORAIRE
+    console.log('apiBaseUrl:', config.apiBaseUrl)
+    console.log('URL appelée:', `${config.apiBaseUrl}/login`)
 
     try {
         const data = await apiFetch<AuthResponse>('/login', {
             method: 'POST',
             body,
         })
-
         setAuthCookies(event, data.token, data.refreshToken)
         return { userId: data.userId, roles: data.roles }
     } catch (e: any) {
+        console.log('Erreur brute:', JSON.stringify(e))
         handleSpringError(e, 'Erreur lors de la connexion')
     }
 })
